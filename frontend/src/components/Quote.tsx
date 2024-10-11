486cf0ec7fddc982b251628657a7c750f98e6d7b
amaan3147 <amanshaikh333333@gmail.com>
2024-10-11 21:47:38 +0530

    type error

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,20 @@
+
+export const Quote = () => {
+    return <div className="bg-slate-300 h-screen flex justify-center flex-col">
+                <div className="flex justify-center">
+                    <div className="max-w-lg ">
+                            <div className="text-3xl font-bold">
+                            Success and failure are both part of life. Both are not permanent.
+                            </div>
+                    
+                
+                        <div className="max-w-md text-xl font-semibold text-left mt-3">
+                        Shah Rukh Khan
+                        </div>
+                        <div className="max-w-md text-sm font-normal text-slate-800">
+                        Actor
+                        </div>
+                    </div>
+                </div>
+            </div>
+}
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://backend.mr-aman3147.workers.dev"
